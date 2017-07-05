8b98a65316e7035b5fc6b7f192c57ed103f95682
Gertautas Mačiūta <gertautasm@infostruktura.lt>
2017-07-05 09:47:26 +0300

    pirkimas

---
+++ php/gavimas.php
@@ -0,0 +1,49 @@
+<?php
+
+$errorMSG = "";
+
+// NAME
+if (empty($_POST["name"])) {
+    $errorMSG = "Reikalingas vardas ";
+} else {
+    $name = $_POST["name"];
+}
+
+// MESSAGE
+if (empty($_POST["message"])) {
+    $errorMSG .= "Žinutė negali būti tuščia ";
+} else {
+    $message = $_POST["message"];
+}
+
+
+$EmailTo = "gmaciuta@gmail.com";
+$Subject = "Gauta nauja žinutė";
+$emailas = "kazkoksemailas@siusti.com";
+
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$emailas);
+
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Kažkas blogai :(";
+    } else {
+        echo $errorMSG;
+        
+    }
+}
+
+?>
